2bb203e24502ad3f6bbd192fc92e839d65f58fd0
Robert21AV <138814776+Robert21AV05@users.noreply.github.com>
2024-08-03 17:47:28 -0500

    Changing lineage_dipper and adding AlphaDroid Flags

--- lineage_dipper.mk
+++ lineage_dipper.mk
@@ -25,3 +25,54 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT="dipper"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# FLAGS
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
+
+# Device config
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GAPPS
+WITH_GMS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+
+# Maintainer
+ALPHA_BUILD_TYPE := Unofficial
+ALPHA_MAINTAINER := Robert21AV
+
+# Device config
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 3
+
+# TARGET_LAUNCHER options:
+# 1 - stock (default)
+# 2 - lawnchair
+# 3 - pixel (valid only on gapps builds)
+TARGET_LAUNCHER := 2
+
+# GAPPS (valid only for GAPPS builds)
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+# Dex/ART optimization
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+USE_DEX2OAT_DEBUG := false
